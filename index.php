14fe553d15c74fd67387e3d8c57c4dd3256e76c9
COMLAB <>
2025-02-12 17:58:14 +0800

    Updated w img
    haha may posa

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Cat meme</title>
 </head>
 <body>
     <h1>
         <p> Hello world</p>
+        <img src="pusa.jfif" alt="">
     </h1>            
 </body>
 </html>
